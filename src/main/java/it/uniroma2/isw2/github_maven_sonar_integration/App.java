cc0770607f73729d043145d80a33690ae011e707
Simone <tib.sim.98@gmail.com>
2021-03-27 17:24:23 +0100

    #1

---
+++ src/main/java/it/uniroma2/isw2/github_maven_sonar_integration/App.java
@@ -0,0 +1,17 @@
+package it.uniroma2.isw2.github_maven_sonar_integration;
+
+import java.util.logging.Logger;
+
+/**
+ * Main class of the project
+ * @author Simone Tiberi (M. 0299908)
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(App.class.getName());
+		logger.info("Hello, World!");
+		logger.info("Hope for correct integration of GitHub, Maven and SonarCloud");
+	}
+}
---
+++ src/test/java/it/uniroma2/isw2/github_maven_sonar_integration/AppTest.java
@@ -0,0 +1,18 @@
+package it.uniroma2.isw2.github_maven_sonar_integration;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Example of test class
+ * @author Simone Tiberi (M. 0299908)
+ *
+ */
+public class AppTest {
+	
+    @Test
+    public void shouldAnswerWithTrue() {
+    	assertTrue(true);
+    }
+}
